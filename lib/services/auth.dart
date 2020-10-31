5b564df086a9013863e8fac748cd2c7314ec1deb
Vasu Gupta <vasu23gupta@gmail.com>
2020-10-31 22:52:30 +0530

    vid 6 :

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,24 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:test_proj/models/customUser.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //custom user based on firebase user
+  CustomUser _userFromFirebaseUser(User user) {
+    if (user != null) {
+      return CustomUser(uid: user.uid);
+    } else {
+      return null;
+    }
+  }
 
   //sign in anon
   Future signInAnon() async {
     try {
       UserCredential result = await _auth.signInAnonymously();
       User user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
