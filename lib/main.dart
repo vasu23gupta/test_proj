b3ae2134439356cbc2e26bf1dd1a22c45a003b8c
Vasu Gupta <vasu23gupta@gmail.com>
2020-10-31 23:21:16 +0530

    vid 8

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:test_proj/models/customUser.dart';
 import 'package:test_proj/screens/wrapper.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:test_proj/services/auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,8 +15,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<CustomUser>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:test_proj/models/customUser.dart';
 import 'package:test_proj/screens/authenticate/authenticate.dart';
 import 'package:test_proj/screens/home/home.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<CustomUser>(context);
+    print(user);
     //return either home or authenticate
-
     return Authenticate();
   }
 }
