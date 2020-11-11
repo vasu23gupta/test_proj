2579d78d5282cdb516d8366148bf02ba0f7d778d
Vasu Gupta <vasu23gupta@gmail.com>
2020-11-11 23:51:58 +0530

    added search bar

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:test_proj/models/appUser.dart';
+import 'package:test_proj/models/customUser.dart';
 import 'package:test_proj/screens/home/add_vendor.dart';
 import 'package:test_proj/services/auth.dart';
 import 'package:test_proj/services/database.dart';
@@ -23,79 +24,122 @@ class _HomeState extends State<Home> {
   final AuthService _auth = AuthService();
   MapController controller = new MapController();
   LatLng userLoc = new LatLng(28.612757, 77.230445);
+  Icon appBarIcon = Icon(Icons.search);
+  dynamic appBarTitle = Text('Map');
+  String stringToSearch;
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<CustomUser>(context);
     List vendors = Provider.of<List<Vendor>>(context);
-    vendors.forEach((element) {
-      print(element.id);
-      print(element.name);
-      print(element.coordinates.toString());
-      print(element.tags.toString());
-    });
+    // vendors.forEach((element) {
+    //   print(element.id);
+    //   print(element.name);
+    //   print(element.coordinates.toString());
+    //   print(element.tags.toString());
+    // });
     LocationService locSer = new LocationService();
     Future<LatLng> userLocFut = locSer.getLocation();
-    return StreamProvider<List<AppUser>>.value(
-      value: UserDatabaseService().users,
-      child: Scaffold(
-        backgroundColor: Colors.brown[50],
-        appBar: AppBar(
-          title: Text('Map'),
-          backgroundColor: Colors.brown[400],
-          elevation: 0.0,
-          actions: <Widget>[
-            FlatButton.icon(
-              icon: Icon(Icons.person),
-              label: Text('logout'),
-              onPressed: () async {
+    return Scaffold(
+      backgroundColor: Colors.brown[50],
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text(user.uid),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Logout'),
+              onTap: () async {
                 await _auth.signOut();
               },
-            )
-          ],
-        ),
-        body: new FlutterMap(
-          mapController: controller,
-          options: new MapOptions(
-            zoom: 13.0,
-            center: userLoc,
-            //center: new LatLng(userLoc.latitude, userLoc.longitude),
-          ),
-          layers: [
-            new TileLayerOptions(
-              urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-              subdomains: ['a', 'b', 'c'],
             ),
-            // new MarkerLayerOptions(
-            //   markers: [
-            //     new Marker(
-            //       width: 45.0,
-            //       height: 45.0,
-            //       //point: new LatLng(28.612757, 77.230445),
-            //       point: userLoc,
-            //       builder: (ctx) => new Container(
-            //         child: new FlutterLogo(),
-            //       ),
-            //     ),
-            //   ],
-            // ),
           ],
         ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.location_searching),
-          onPressed: () {
-            // userLocFut.then((value) => setState(() {
-            //       userLoc = new LatLng(value.latitude, value.longitude);
-            //       controller.move(userLoc, 13.0);
-            //       print(userLoc);
-            //     }));
-
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => AddVendor())
-                // MaterialPageRoute(builder: (context) => StreamProvider<List<Vendor>>.value(
-                //   value: VendorDatabaseService().vendors,
-                //   child:AddVendor())),
-                );
-          },
+      ),
+      appBar: AppBar(
+        title: appBarTitle,
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: appBarIcon,
+            onPressed: () {
+              setState(
+                () {
+                  if (this.appBarIcon.icon == Icons.search) {
+                    this.appBarIcon = new Icon(Icons.close);
+                    this.appBarTitle = new TextField(
+                      // onChanged: (value) {
+                      //   setState(() {
+                      //     stringToSearch = value;
+                      //   });
+                      // },
+                      style: new TextStyle(
+                        color: Colors.white,
+                      ),
+                      decoration: new InputDecoration(
+                          prefixIcon:
+                              new Icon(Icons.search, color: Colors.white),
+                          hintText: "Search...",
+                          hintStyle: new TextStyle(color: Colors.white)),
+                    );
+                  } else {
+                    this.appBarIcon = new Icon(Icons.search);
+                    this.appBarTitle = new Text("AppBar Title");
+                  }
+                },
+              );
+            },
+          ),
+        ],
+      ),
+      body: new FlutterMap(
+        mapController: controller,
+        options: new MapOptions(
+          zoom: 13.0,
+          center: userLoc,
+          //center: new LatLng(userLoc.latitude, userLoc.longitude),
         ),
+        layers: [
+          new TileLayerOptions(
+            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            subdomains: ['a', 'b', 'c'],
+          ),
+          // new MarkerLayerOptions(
+          //   markers: [
+          //     new Marker(
+          //       width: 45.0,
+          //       height: 45.0,
+          //       //point: new LatLng(28.612757, 77.230445),
+          //       point: userLoc,
+          //       builder: (ctx) => new Container(
+          //         child: new FlutterLogo(),
+          //       ),
+          //     ),
+          //   ],
+          // ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.location_searching),
+        onPressed: () {
+          // userLocFut.then((value) => setState(() {
+          //       userLoc = new LatLng(value.latitude, value.longitude);
+          //       controller.move(userLoc, 13.0);
+          //     }));
+
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => AddVendor())
+              // MaterialPageRoute(builder: (context) => StreamProvider<List<Vendor>>.value(
+              //   value: VendorDatabaseService().vendors,
+              //   child:AddVendor())),
+              );
+        },
       ),
     );
   }
