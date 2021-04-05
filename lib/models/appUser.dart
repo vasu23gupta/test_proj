efce83c425a1dbf5da14aa593474d2e4766aff61
Swayam221 <68892315+Swayam221@users.noreply.github.com>
2021-04-05 18:57:23 +0530

    added points system and profile page

--- lib/models/appUser.dart
+++ lib/models/appUser.dart
@@ -1,5 +1,15 @@
 class AppUser {
-  final String name;
+  String name;
+  int level;
+  int points;
+  int nextLevelAt;
 
-  AppUser({this.name});
+  //AppUser({this.name,this.level,this.points,this.nextLevelAt})
+
+  AppUser(Map<String, dynamic> json){
+    this.level=json['level'];
+    this.points=json['points'];
+    this.nextLevelAt=json['nextLevelAt'];
+    this.name=json['username'];
+  }  
 }
