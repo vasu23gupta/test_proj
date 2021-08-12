478435282d40b7317daf09dd439771f6f2665f50
Vasu Gupta <vasu23gupta@gmail.com>
2021-08-12 15:49:01 +0530

    completed about us

--- lib/screens/about us/aboutus.dart
+++ lib/screens/about us/aboutus.dart
@@ -7,144 +7,71 @@ class AboutUsPage extends StatefulWidget {
   _AboutUsState createState() => _AboutUsState();
 }
 
-class _AboutUsState extends State<AboutUsPage>{
-
- Size _size;
-
-  @override
-  void initState(){
-
-  }
+class _AboutUsState extends State<AboutUsPage> {
+  Size _size;
 
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     _size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text("About Us"),
-        /*backgroundColor: BACKGROUND_COLOR,*/
+        backgroundColor: BACKGROUND_COLOR,
       ),
-      
-    body: SingleChildScrollView(
-        child: Column(
-           crossAxisAlignment: CrossAxisAlignment.start,
-             children: <Widget>[
-                Container(
-                   padding: EdgeInsets.fromLTRB(8, 2, 8, 2),
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.1),
-              spreadRadius: 5,
-              blurRadius: 2,
-              offset: Offset(0, 5), // changes position of shadow
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: <Widget>[
+            Text(
+              "Authors",
+              style: TextStyle(fontSize: _size.width * 0.1),
+            ),
+            ListTile(
+              title: Text(
+                "Vasu Gupta",
+                style: TextStyle(fontSize: _size.width * 0.05),
+              ),
+              onTap: () async {
+                const String url =
+                    "https://www.linkedin.com/in/vasu-gupta-677454194";
+                if (await canLaunch(url)) {
+                  await launch(url);
+                } else {
+                  throw "cannot load url";
+                }
+              },
+            ),
+            ListTile(
+              title: Text(
+                "Divit Goel",
+                style: TextStyle(fontSize: _size.width * 0.05),
+              ),
+              onTap: () async {
+                const String url = "https://www.linkedin.com/in/divit-goel";
+                if (await canLaunch(url)) {
+                  await launch(url);
+                } else {
+                  throw "cannot load url";
+                }
+              },
+            ),
+            ListTile(
+              title: Text(
+                "Swayam Gupta",
+                style: TextStyle(fontSize: _size.width * 0.05),
+              ),
+              onTap: () async {
+                const String url = "https://www.linkedin.com/in/swayam221";
+                if (await canLaunch(url)) {
+                  await launch(url);
+                } else {
+                  throw "cannot load url";
+                }
+              },
             ),
           ],
-        ),child: ListTile(
-          tileColor: Theme.of(context).cardColor,
-          onTap: () =>{
-
-          },
-          title: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(4.0),
-              ),
-             /* Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                Image(image: AssetImage('assets/vasu.jfif'),
-                 fit: BoxFit.cover,
-                        height: _size.height * 1,
-                        width: _size.width * 1)
-                
-                ],
-              ),*/
-            Column(
-              children: [
-                Row(
-                  
-                 mainAxisSize: MainAxisSize.min,
-                 children: [
-                   Text("     Authors", style: TextStyle(fontSize:_size.width * 0.12, ),)
-                 ],
-                ),
-                 Row(
-                 
-                 mainAxisSize: MainAxisSize.min,
-                children: [
-                  SizedBox(
-                    child:  RichText(text: TextSpan(children: [TextSpan(text: "      Vasu Gupta",style: TextStyle(fontSize:  _size.width * 0.08,),
-                  recognizer: TapGestureRecognizer()..onTap= () async {
-                    var url = "https://www.linkedin.com/in/vasu-gupta-677454194";
-                    if(await canLaunch(url)){
-                      await launch(url);
-                    }
-                    else{
-                      throw "cannot load url";
-                    }
-                  },
-                  ),
-                  ])),
-                  ),
-                ],
-              ),
-                  
-               Row(
-               
-                 mainAxisSize: MainAxisSize.min,
-                children: [
-                  SizedBox(
-                    child:  RichText(text: TextSpan(children: [TextSpan(text: "     Divit Goel",style: TextStyle(fontSize:  _size.width * 0.08,),
-                  recognizer: TapGestureRecognizer()..onTap= () async {
-                    var url = "https://www.linkedin.com/in/divit-goel";
-                    if(await canLaunch(url)){
-                      await launch(url);
-                    }
-                    else{
-                      throw "cannot load url";
-                    }
-                  },
-                  ),
-                  ])),
-                  ),
-                ],
-              ),
-             
-               Row(
-                
-                 mainAxisSize: MainAxisSize.min,
-                children: [
-                  SizedBox(
-                    child:  RichText(text: TextSpan(children: [TextSpan(text: "      Swayam Gupta",style: TextStyle(fontSize:  _size.width * 0.08),
-                  recognizer: TapGestureRecognizer()..onTap= () async {
-                    var url = "https://www.linkedin.com/in/swayam221";
-                    if(await canLaunch(url)){
-                      await launch(url);
-                    }
-                    else{
-                      throw "cannot load url";
-                    }
-                  },
-                  ),
-                  ])),
-                  ),
-                ],
-              ),
-              
-              ],
-            ),
-           
-            ],
-          ),
         ),
-      )
-                
-             ]
-        )
-    )
+      ),
     );
-
   }
 }
